03a9b09137fe533dd4bc406df90f7da650baf203
fujimoto <dai.fuji.110728@gmail.com>
2022-03-12 18:36:59 +0900

    add s3

---
+++ modules/s3/main.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "backet" {
+  bucket = "${var.name}-${var.env}-mybacket"
+
+  tags = {
+    Name = "${var.name}-${var.env}-mybacket"
+  }
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.backet.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket                  = aws_s3_bucket.backet.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
