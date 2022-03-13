b83e4ade683e73d60936a967923533fe8df0b2de
fujimoto <dai.fuji.110728@gmail.com>
2022-03-13 18:49:35 +0900

    make credential

--- main.tf
+++ main.tf
@@ -25,9 +25,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
+  profile = "terraform" #[aws config --profile ***]で名前付きプロファイルを作成
 }
 
 #-----------------------------------------------------------------------------------------
